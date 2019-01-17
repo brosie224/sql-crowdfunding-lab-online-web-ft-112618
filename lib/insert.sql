7ddf21c368ab01edf5c2f61842e477ed9391cfc6
Brian Rosengarten <barosengarten@gmail.com>
2019-01-17 06:06:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,24 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Marianna", "Model", 50000, "1/1/2017", "1/17/2018");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Marianna", "Model", 50000, "1/1/2017", "1/17/2018");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Marianna", "Model", 50000, "1/1/2017", "1/17/2018");
+
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
+INSERT INTO users (name, age) VALUES ("Marianna", 33);
