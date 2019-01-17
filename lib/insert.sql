2a9be2d8d8d80ef49ec84e24103bfea3bd506da1
Brian Rosengarten <barosengarten@gmail.com>
2019-01-17 05:39:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -16,5 +16,5 @@ INSERT INTO projects VALUES ("Marianna", "Model", "50000", "1/1/2017", "1/17/201
 INSERT INTO projects VALUES ("Marianna", "Model", "50000", "1/1/2017", "1/17/2018");
 INSERT INTO projects VALUES ("Marianna", "Model", "50000", "1/1/2017", "1/17/2018");
 INSERT INTO projects VALUES ("Marianna", "Model", "50000", "1/1/2017", "1/17/2018");
-INSERT INTO projects VALUES ("Marianna", "Model", "50000", "1/1/2017", "1/17/2018")
+INSERT INTO projects VALUES ("Marianna", "Model", "50000", "1/1/2017", "1/17/2018");
 INSERT INTO projects VALUES ("Marianna", "Model", "50000", "1/1/2017", "1/17/2018");
