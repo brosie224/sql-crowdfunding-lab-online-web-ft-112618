5b4f9a095a5aa864a56e06b7c3ee7257656a369b
Brian Rosengarten <barosengarten@gmail.com>
2019-01-17 07:36:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,9 +28,9 @@ HAVING amount_over_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount FROM users
+"SELECT users.name, SUM(pledges.amount) FROM users
 JOIN pledges ON users.id = pledges.user_id
-GROUP BY users.name
+GROUP BY users.id
 ORDER BY pledges.amount;"
 end
 
